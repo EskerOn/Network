ff56aa49ae94bef878b3a8891e2457946aee8e1e
EskerOn <35613070+EskerOn@users.noreply.github.com>
2020-05-05 17:50:11 -0500

    Update and rename writter.py to writer.py

--- writer.py
+++ writer.py
@@ -12,7 +12,8 @@ pieces = len(aux)//100
 i = 0
 buffera = ""
 bufferb = ""
-def generarTrama(i, buffera, bufferb):
+bufferc = ""
+def generarTrama(i, buffera, bufferb, bufferc):
 	h = hex(i+1)
 	buffera += chr(1)+h+chr(2)+"{}".format(aux[i*100:(i+1)*100])+chr(3)+"FF"+"\n"
 	print("formato de trama")
@@ -22,22 +23,30 @@ def generarTrama(i, buffera, bufferb):
 	stringas = ""
 	for trama in ascii_char:
 		stringas+=str(trama)
-	bufferb+= "1"+str((int(h, 16)))+"2"+"{}".format(stringas)+"3"+str(int("0xFF", 16))+"\n"
+	temp = "1"+str((int(h, 16)))+"2"+"{}".format(stringas)+"3"+str(int("0xFF", 16))+"\n"
+	bufferb+=temp
 	print(bufferb)
 	print("\n")
+	print("con codigo de error")
+	temp=temp[:len(temp)-4]
+	temp=temp+str(checksum256(temp))
+	bufferc+=temp
+	print(bufferc)
 
 def checksum256(st):
     return functools.reduce(lambda x,y:x+y, map(ord, st)) % 256
 while(i < pieces):
-	generarTrama(i, buffera, bufferb)
+	generarTrama(i, buffera, bufferb, bufferc)
 	i += 1
 if rest>0:
-	generarTrama(i, buffera, bufferb)
+	generarTrama(i, buffera, bufferb, bufferc)
 
 f = open('DocumentoFormat.txt', "w")
 e = open('DocumentoFormatASCII.txt', "w")
+g = open('DocumentoCodigoError.txt', 'w')
 f.write(buffera)
 e.write(bufferb)
-
+g.write(bufferc)
 e.close()
 f.close()
+g.close()
